c46af973cf1a85a1d4cdab9442289ff673646394
philips-of-ng@philips <philipsedun@gmail.com>
2025-02-25 02:56:49 +0100

    good morning

--- src/context/AuthContext.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { createContext, useContext, useEffect, useState } from "react";
-import { auth } from "../firebaseConfig";
-import { onAuthStateChanged, signOut } from "firebase/auth";
