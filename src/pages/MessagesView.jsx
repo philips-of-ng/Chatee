76a50e5e6608c80ec9decfc5671a972bd56f3d97
philips-of-ng@philips <philipsedun@gmail.com>
2025-03-31 04:44:36 +0100

    byeee

--- src/pages/MessagesView.jsx
+++ src/pages/MessagesView.jsx
@@ -7,11 +7,31 @@ import OneListedChat from '../components/OneListedChat.jsx'
 const MessagesView = () => {
   return (
     <div className='messages-view'>
-      
+
       <div className='add-user'>
         <div className='add-user-top'>
           <button><i class='bx bx-arrow-back'></i></button>
           <input type="text" placeholder='Search for a friend...' />
+        </div>
+
+        <div className='add-user-main'>
+
+          <div className='one-result'>
+            <img src={assets.userImage} alt="" />
+
+            <div className='or-right'>
+              <div>
+                <p>Philips Edun</p>
+                <span>Frontend Dev, YBNL.</span>
+              </div>
+              <div>
+                <button>
+                  <i class='bx bx-message-dots'></i>
+                </button>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
 
